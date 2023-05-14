a11fc825d1557181f03918ed86866ce47695c06a
Nacho <roncalnacho98@gmail.com>
2023-05-14 10:50:37 -0300

    Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'admin', component: AdminPageComponent },
+]
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+
 import { EducationComponent } from './components/education/education.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HabilitiesComponent } from './components/habilities/habilities.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { RecentProjectsComponent } from './components/recent-projects/recent-projects.component';
+import { AppRoutingModule } from './app-routing.module';
 
-import { HttpClientModule } from '@angular/common/http';
-import { MainPageComponent } from './components/main-page/main-page.component';
-import { LoginPageComponent } from './components/login-page/login-page.component';
-import { AdminPageComponent } from './components/admin-page/admin-page.component';
 
 @NgModule({
   declarations: [
